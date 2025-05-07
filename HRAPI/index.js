01b11945c4e181dcdf8eb04da82892c403054496
Mahad-Sardar <bscs2312159@szabist.pk>
2025-05-07 13:15:47 +0000

    GET METHOD

--- HRAPI/index.js
+++ HRAPI/index.js
@@ -47,6 +47,63 @@ app.get('/count',async(req,res)=>{
         res.status(500).json({Error:err.message})
     }
 });
+app.get('/employee',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from employees');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message})
+    }
+})
+app.get('/job',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from jobs');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message})
+    }
+})
+app.get('/department',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from department');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message})
+    }
+})
+app.get('/location',async(req,res)=>{
+    try{
+        const result = await pool.query('select * from locations');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message})
+    }
+})
+app.get('/totalcrty',async(req,res)=>{
+    try{
+        const result = await pool.query('select count(country_id) from countries');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message})
+    }
+})
+app.get('/totaldpt',async(req,res)=>{
+    try{
+        const result = await pool.query('select count(department_id) from departments');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message})
+    }
+})
+app.get('/totaldpt',async(req,res)=>{
+    try{
+        const result = await pool.query('select count(department_id) from departments');
+        res.json(result.rows);
+    }catch(err){
+        res.status(500).json({Error:err.message})
+    }
+})
+
 
 
 const PORT = process.env.PORT || 6005;
